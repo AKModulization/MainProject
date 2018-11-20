446fe3e15ad600f1f03f6cb23a29b39caccb8b84
eric <ericoda@126.com>
2018-11-20 23:54:21 +0800

    Initial Commit

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by EricLau on 2018/11/20.
+//  Copyright © 2018年 Anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by EricLau on 2018/11/20.
+//  Copyright © 2018年 Anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
